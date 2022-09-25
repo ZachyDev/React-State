4f878fe01808a1ec849192ae46e081c76e370cdc
= <=>
2022-09-25 14:59:44 +0300

    :white_check_mark: validating forms in react

--- tech-house/src/components/Signup.js
+++ tech-house/src/components/Signup.js
@@ -1,11 +1,12 @@
 import React,  {useState } from 'react'
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 function Signup() {
   const [firstName,setFirstName] = useState('John');
   const [lastName,setLastName] = useState('Doe');
   const [submittedData,setSubmittedData] = useState([]);
-
+  const [error,setError] = useState('');
 //   handle first name change
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -18,11 +19,16 @@ function Signup() {
 //   handle submit
 function handleSubmit(event) {
     event.preventDefault();
-    const userData = {firstName: firstName, lastName: lastName}
-    const usersArray = [...submittedData, userData];
-    setSubmittedData(usersArray);
-    setFirstName("");
-    setLastName("")
+    if(firstName && lastName) {
+        const userData = {firstName: firstName, lastName: lastName}
+        const usersArray = [...submittedData, userData];
+        setSubmittedData(usersArray);
+        setFirstName("");
+        setLastName("")
+    }
+    else {
+        setError("Both first and last name are required");
+    }
 }
     // display registered users
     const listAllUsers = submittedData.map((user,index) => {
@@ -34,12 +40,13 @@ function handleSubmit(event) {
     })
   return (
     <div>
+        { error }
         <form onSubmit={handleSubmit}>
             <h4 >Sign Up</h4>
-           
             <input type="text" placeholder='Enter First Name' onChange={handleFirstNameChange}/>
             <input type="text" placeholder='Enter Last Name' onChange={handleLastNameChange}/>
             <Button type='submit'>Sign up</Button>
+
         </form>
         { listAllUsers }
     </div>
